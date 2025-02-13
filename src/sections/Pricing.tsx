99e1407d8376da8b25fc0b686291265e0cb857a3
DevTyler62 <smithtj921@gmail.com>
2025-02-13 16:08:44 -0500

    updated minor text issues

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -110,7 +110,7 @@ export const Pricing = () => {
                         }}
                         className="bg-[linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] text-transparent bg-clip-text font-medium"
                       >
-                        Popular
+                        Most Popular
                       </motion.span>
                     </div>
                   )}
